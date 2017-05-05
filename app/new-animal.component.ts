5088e8511def178ca3bc79389558bbc0fd5b86d9
Riley Watts <rileywatts.yes@gmail.com>
2017-05-05 09:53:46 -0700

    add form to new-animal.component

--- app/new-animal.component.ts
+++ app/new-animal.component.ts
@@ -7,6 +7,43 @@ import { Animal } from './animal.model';
   <div class="newAnimal">
     <h1> Lets make an animal </h1>
     <form class="animalForm">
+      <div class="form-group">
+        <label for="species">Species</label>
+        <input type="text" id="species" #species>
+      </div>
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" id="name" #name>
+      </div>
+      <div class="form-group">
+        <label for="age">Age</label>
+        <input type="number" id="age" #age>
+      </div>
+      <div class="form-group">
+        <label for="diet">Diet</label>
+        <input type="text" id="diet" #diet>
+      </div>
+      <div class="form-group">
+        <label for="location">Location</label>
+        <input type="text" id="location" #location>
+      </div>
+      <div class="form-group">
+        <label for="caretakers">Caretakers</label>
+        <input type="number" id="caretakers" #caretakers>
+      </div>
+      <div class="form-group">
+        <label for="sex">Sex</label>
+        <input type="text" id="sex" #sex>
+      </div>
+      <div class="form-group">
+        <label for="likes">Likes</label>
+        <input type="text" id="likes" #likes>
+      </div>
+      <div class="form-group">
+        <label for="dislikes">Dislikes</label>
+        <input type="text" id="dislikes" #dislikes>
+      </div>
+      <button (click)="submitForm(species)"
     </form>
     </div>
     `
