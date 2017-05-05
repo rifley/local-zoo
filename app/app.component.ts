80a9e4a6bb10df50ee474043b96bf2678b60b5fd
Riley Watts <rileywatts.yes@gmail.com>
2017-05-05 09:29:57 -0700

    create animal-list component and add hardcoded animal array to app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
   template: `
   <h1>Welcome to the zoo motherfucker</h1>
   <new-animal></new-animal>
+  <animal-list [childAnimalList]="animals"></animal-list>
   `
 })
 
 export class AppComponent {
+  animals: Animal[] = [
+    new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
+    new Animal('Ocelot', 'Prince', 4, 'Carnivore', 'Tropical Rain Forest Building', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based')
+  ];
+
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { NewAnimalComponent } from './new-animal.component';
+import { AnimalListComponent } from './animal-list.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [ AppComponent, NewAnimalComponent ],
+  declarations: [ AppComponent, NewAnimalComponent, AnimalListComponent ],
   bootstrap:    [ AppComponent ]
 })
 
